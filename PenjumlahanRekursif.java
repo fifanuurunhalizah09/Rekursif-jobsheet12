c2c2635a3434d66711c4590531c2f7983a594bb7
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-11-28 13:32:55 +0700

    soalPecobaan3

---
+++ PenjumlahanRekursif.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PenjumlahanRekursif {
+    static int hitungPenjumlahan(int n, StringBuilder deret) {
+        if (n == 0) {
+            return 0;  
+        } else {
+            int result = n + hitungPenjumlahan(n - 1, deret);
+
+            if (n > 1) {
+                deret.insert(0, n + "+"); 
+            } else {
+                deret.insert(0, n);
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int f;
+
+     
+        System.out.print("Masukkan nilai f: ");
+        f = sc.nextInt();
+
+
+        StringBuilder deret = new StringBuilder();
+
+        int hasil = hitungPenjumlahan(f, deret);
+        System.out.println("Deret penjumlahan: " + deret + " = " + hasil);
+    }
+}
